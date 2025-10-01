ef4f7fee6aa8aa01c9e2269d9a4916e5fd71ed20
ampertoka <spolt888@gmail.com>
2025-10-01 22:11:23 +0300

    домз4

--- main.py
+++ main.py
@@ -8,8 +8,33 @@ app = FastAPI()
 static_dir = (Path(__file__).parent / "static")
 app.mount("/ui", StaticFiles(directory=str(static_dir), html=True), name="ui")
 
-@app.get('/test')
-def test():
-    dict_f = {'name': 'serg', 'adress': 'moscow', 'house': 7, 'data': '26:09:2025'}
+dict_f = [{'name': 'cable', 'sechenie': '1.5', 'color': 'red', 'data': '26:09:2025'},
+          {'name': 'cable', 'sechenie': '2.5', 'color': 'red', 'data': '26:09:2025'}]
+
+
+@app.get('/cat')
+def catalog():
     print('сказать повару: быстрее')
+    return dict_f
+
+
+@app.post('/cat/add')
+def catalog_add():
+    dict_f.append(
+        {'name': 'cable', 'sechenie': '2.5', 'color': 'green', 'data': '26:09:2025'}
+    )
+    return dict_f
+
+
+@app.delete('/cat/del')
+def catalog_delete():
+    del dict_f[0]
+    return dict_f
+
+
+@app.get('/cat/update')
+def catalog_update():
+    print(dict_f[-1])
+    dict_f[-1]['color'] = 'black'
+    print(dict_f[-1])
     return dict_f
